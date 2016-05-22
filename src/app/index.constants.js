128d94d08791d33801380469617977482a3f84f1
Fernando Falci <falci@falci.me>
2016-05-22 06:28:47 -0300

    removed logger; fix https issue; using our server

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -7,9 +7,8 @@
     .module('mastermindUi')
     .constant('malarkey', malarkey)
     .constant('moment', moment)
-    // .constant('api', 'https://192.168.100.10:8080/mastermind/v1')
-    .constant('api', 'https://az-mastermind.herokuapp.com')
-    // .constant('api', 'http://mastermind-pugstunt.herokuapp.com/mastermind/v1')
+    // .constant('api', 'https://az-mastermind.herokuapp.com')
+    .constant('api', 'https://mastermind-pugstunt.herokuapp.com/mastermind/v1')
     .constant('colors', {
       R: 'red',
       B: 'blue',
